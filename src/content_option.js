bda28c81ef43c920b673325fb33dae5a0b6a8f51
kevinp83 <kevin.mail.pro@gmail.com>
2023-09-06 16:57:27 +0200

    Dépot du projet

---
+++ src/content_option.js
@@ -0,0 +1,149 @@
+const logotext = "Kevin";
+const meta = {
+    title: "Kevin.P, Portfolio",
+    description: "Bonjour, je suis Kevin, développeur Frontend React débutant ! N'hésitez pas à me contacter pour votre projet.",
+};
+
+const introdata = {
+    title: "Je suis Kevin",
+    animated: {
+        first: "J'adore le code et les voyages",
+        second: "J'aimerai lier les deux",
+        third: "Je développe avec React",
+    },
+    description: "De mon amour pour le voyage, m'est venu la passion du code. Pour moi le métier de développeur est le meilleur moyen de pouvoir allier passion et travail. J'engage donc toute mon énergie dans les sites que je développe pour avancer vers une autonomie maximale en freelance.",
+    your_img_url: "IMG_20190907_133935.jpg",
+};
+
+const dataabout = {
+    title: "A propos de moi",
+    aboutme: "Je suis débutant en développement (Formation fini septembre 2023) mais j'apprends très vite. Je suis fiable et m'engage à faire le nécessaire pour concrétiser les projets conformes aux attentes clients. Bien que novice, je suis rapide et éfficace.",
+};
+
+const skills = [{
+        name: "HTML, CSS",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "WordPress",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Node.js",
+        value: 70,
+    },
+    {
+        name: "Express et MongoDB",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Site complet",
+        description: "Je peux vous créer un site sur mesure complet, responsive et qui répond à vos attentes. En code HTML CSS Javascript ou React.",
+    },
+    {
+        title: "Site WordPress",
+        description: "Je peux créer des sites sur WordPress, avec des personnalisations à la demande.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kevin.mail.pro@gmail.com",
+    YOUR_FONE: "06.36.07.65.36",
+    description: "Pour toute question n'hésitez pas à me contacter (par mail de préférence pour le premier contact, réponse plus rapide). Je répond sous 24h maximum (sauf week-end).",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kevinp83",
+    facebook: "https://www.facebook.com/profile.php?id=61550935532925",
+    linkedin: "https://www.linkedin.com/in/kevin-petoux-ba35481a7/",
+    twitter: "https://twitter.com/KevinPetoux",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
